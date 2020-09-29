5f0c67e2f1ad1154ce9229d517b21648cbb94820
OctaveOliviers <octave.oliviers@gmail.com>
2020-09-29 13:46:37 +0100

    initial commit

---
+++ environment.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# @Author: OctaveOliviers
+# @Date:   2020-09-27 18:46:29
+# @Last Modified by:   OctaveOliviers
+# @Last Modified time: 2020-09-29 13:46:09
+
+class Environment():
+    """
+    Environment object coordinates interaction between 
+    the agent and the game.
+    """
+
+    self.game_path = "/"
+
+    def __init__(self, arg):
+        self.arg = arg
+        
+
+    def reset():
+        # call game initializer
+
+
+        return state
+
+    # def render():
+    #   pass
+
+    def step(action):
+        pass
